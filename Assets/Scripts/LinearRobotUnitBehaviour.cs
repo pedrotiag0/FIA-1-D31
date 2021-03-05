43973ffbd0ad75fff42e6351752fe28e1320be3b
Pedro Tiago <72353485+pedrotiag0@users.noreply.github.com>
2021-03-05 18:05:47 +0000

    Update LinearRobotUnitBehaviour.cs

--- Assets/Scripts/LinearRobotUnitBehaviour.cs
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -19,13 +19,13 @@ public class LinearRobotUnitBehaviour : RobotUnit
         resouceAngle = resourcesDetector.GetAngleToClosestResource();
         resourceValue = weightResource * resourcesDetector.GetLinearOuput();
 
-        // get sensor data of walls
+        // get sensor data of walls | Meta 1
         wallAngle = blockDetector.GetAngleToClosestObstacle();
         wallValue = weightWall * blockDetector.GetLinearOuput();
 
         // apply to the ball
-        applyForce(resouceAngle, resourceValue);    // go towards resource
-        applyForce(wallAngle, wallValue);           // go away from wall
+        applyForce(resouceAngle, resourceValue);    // go towards resources
+        applyForce(wallAngle, wallValue);           // go away from walls (negative force will be applied in Unity) | Meta 1
 
         
 
