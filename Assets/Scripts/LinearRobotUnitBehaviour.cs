a1ce8426f272d9bfd6995696e18db13989b6e888
Pedro Tiago <72353485+pedrotiag0@users.noreply.github.com>
2021-03-05 17:41:34 +0000

    Create LinearRobotUnitBehaviour.cs

---
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class LinearRobotUnitBehaviour : RobotUnit
+{
+    public float weightResource;
+    public float resourceValue;
+    public float resouceAngle;
+
+    public float weightWall;
+    public float wallValue;
+    public float wallAngle;
+
+    void Update()
+    {
+
+        // get sensor data of resources
+        resouceAngle = resourcesDetector.GetAngleToClosestResource();
+        resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+
+        // get sensor data of walls
+        wallAngle = blockDetector.GetAngleToClosestObstacle();
+        wallValue = weightWall * blockDetector.GetLinearOuput();
+
+        // apply to the ball
+        applyForce(resouceAngle, resourceValue);    // go towards resource
+        applyForce(wallAngle, wallValue);           // go away from wall
+
+        
+
+    }
+
+
+}
+
+
+
+
+
+
