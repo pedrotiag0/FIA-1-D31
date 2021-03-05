33b8da8ca221d4d69c6ef1fb0510a7c0b33c9658
Pedro Tiago <72353485+pedrotiag0@users.noreply.github.com>
2021-03-05 17:41:29 +0000

    Create BlockDetectorScript.cs

---
+++ Assets/Scripts/BlockDetectorScript.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDetectorScript : MonoBehaviour
+{
+
+    public float angleOfSensors = 10f;
+    public float rangeOfSensors = 10f;
+    protected Vector3 initialTransformUp;
+    protected Vector3 initialTransformFwd;
+    public float strength;
+    public float angleToClosestObj;
+    public int numObjects;
+    public bool debugMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        initialTransformUp = this.transform.up;
+        initialTransformFwd = this.transform.forward;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() // META 1
+    {
+        // YOUR CODE HERE
+        ObjectInfo blockObject;
+        blockObject = GetClosestWall();
+        if (blockObject != null)
+        {
+            angleToClosestObj = blockObject.angle;
+            strength = 1.0f / (blockObject.distance + 1.0f); // forca negativa no unity
+        }
+        else
+        { // no object detected
+            strength = 0;
+            angleToClosestObj = 0;
+        }
+    }
+
+    public ObjectInfo GetClosestWall()
+    {
+        ObjectInfo[] a = (ObjectInfo[])GetVisibleObjects("Wall").ToArray();
+        if (a.Length == 0)
+        {
+            return null;
+        }
+        return a[a.Length - 1];
+    }
+
+    public List<ObjectInfo> GetVisibleObjects(string objectTag)
+    {
+        RaycastHit hit;
+        List<ObjectInfo> objectsInformation = new List<ObjectInfo>();
+
+        for (int i = 0; i * angleOfSensors < 360f; i++)
+        {
+            if (Physics.Raycast(this.transform.position, Quaternion.AngleAxis(-angleOfSensors * i, initialTransformUp) * initialTransformFwd, out hit, rangeOfSensors))
+            {
+
+                if (hit.transform.gameObject.CompareTag(objectTag))
+                {
+                    if (debugMode)
+                    {
+                        Debug.DrawRay(this.transform.position, Quaternion.AngleAxis((-angleOfSensors * i), initialTransformUp) * initialTransformFwd * hit.distance, Color.red);
+                    }
+                    ObjectInfo info = new ObjectInfo(hit.distance, angleOfSensors * i + 90);
+                    objectsInformation.Add(info);
+                }
+            }
+        }
+
+        objectsInformation.Sort();
+
+        return objectsInformation;
+    }
+
+    public float GetAngleToClosestObstacle()
+    {
+        return angleToClosestObj;
+    }
+
+    public float GetLinearOuput()
+    {
+        return strength;
+    }
+
+    public virtual float GetGaussianOutput() // META 2
+    {
+        // YOUR CODE HERE
+        throw new NotImplementedException();
+    }
+
+    public virtual float GetLogaritmicOutput() // META 2
+    {
+        // YOUR CODE HERE
+        throw new NotImplementedException();
+    }
+}
